0a74e20c7b2fdb5523d4b7e6c8fba8511933e285
MilosK2 <jano9987@gmail.com>
2024-10-21 13:03:40 +0200

    Funtction correction

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-#include "zadanie.hpp"
+#include "..\include\catch.hpp"
+#include "..\include\zadanie.hpp"
 
 TEST_CASE("Test dodawania") {
   CHECK(dodaj(2, 2) == 4);
